39b49c3e9dd54a4f09739d2aa4fa29c457719304
iandelaney <delaney.ian@gmail.com>
2026-04-23 12:43:06 +0100

    Add regex functions for extracting markdown images and links with unit tests

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,3 +1,5 @@
+import re
+
 from textnode import TextNode, TextType
 
 
@@ -24,3 +26,35 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.append(TextNode(section, text_type))
 
     return new_nodes
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        split_text = old_node.text.split(delimiter)
+
+        if len(split_text) % 2 == 0:
+            raise Exception(f"invalid markdown, formatted section not closed: {delimiter}")
+
+        for i, section in enumerate(split_text):
+            if section == "":
+                continue
+
+            if i % 2 == 0:
+                new_nodes.append(TextNode(section, TextType.TEXT))
+            else:
+                new_nodes.append(TextNode(section, text_type))
+
+    return new_nodes
+
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,7 +1,11 @@
 import unittest
 
 from textnode import TextNode, TextType
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import (
+    split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links,
+)
 
 
 class TestSplitNodesDelimiter(unittest.TestCase):
@@ -84,6 +88,60 @@ class TestSplitNodesDelimiter(unittest.TestCase):
             ],
         )
 
+class TestMarkdownExtraction(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual(
+            [("image", "https://i.imgur.com/zjjcJKZ.png")],
+            matches,
+        )
+
+    def test_extract_markdown_images_multiple(self):
+        matches = extract_markdown_images(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) "
+            "and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        )
+        self.assertListEqual(
+            [
+                ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+                ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"),
+            ],
+            matches,
+        )
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev)"
+        )
+        self.assertListEqual(
+            [("to boot dev", "https://www.boot.dev")],
+            matches,
+        )
+
+    def test_extract_markdown_links_multiple(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) "
+            "and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual(
+            [
+                ("to boot dev", "https://www.boot.dev"),
+                ("to youtube", "https://www.youtube.com/@bootdotdev"),
+            ],
+            matches,
+        )
+
+    def test_extract_markdown_links_ignores_images(self):
+        matches = extract_markdown_links(
+            "Here is an image ![alt](https://example.com/image.png) "
+            "and a link [site](https://example.com)"
+        )
+        self.assertListEqual(
+            [("site", "https://example.com")],
+            matches,
+        )
 
 if __name__ == "__main__":
     unittest.main()
