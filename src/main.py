d1add53739b206e8565c2edfa8a9291fa4ce73b9
iandelaney <delaney.ian@gmail.com>
2026-04-28 09:11:40 +0100

    Add file and directory management functions, new CSS styles, and image asset

--- src/main.py
+++ src/main.py
@@ -1,9 +1,49 @@
-from textnode import TextNode, TextType
+import os
+
+
+def copy_file(src_path, dst_path):
+    with open(src_path, "rb") as src_file:
+        contents = src_file.read()
+
+    with open(dst_path, "wb") as dst_file:
+        dst_file.write(contents)
+
+
+def clear_directory(path):
+    for item in os.listdir(path):
+        item_path = os.path.join(path, item)
+
+        if os.path.isfile(item_path):
+            os.remove(item_path)
+        else:
+            clear_directory(item_path)
+            os.rmdir(item_path)
+
+
+def copy_directory(src, dst):
+    if not os.path.exists(src):
+        raise Exception(f"Source directory does not exist: {src}")
+
+    if os.path.exists(dst):
+        clear_directory(dst)
+    else:
+        os.mkdir(dst)
+
+    for item in os.listdir(src):
+        src_path = os.path.join(src, item)
+        dst_path = os.path.join(dst, item)
+
+        if os.path.isfile(src_path):
+            print(f"Copying file: {src_path} -> {dst_path}")
+            copy_file(src_path, dst_path)
+        else:
+            os.mkdir(dst_path)
+            copy_directory(src_path, dst_path)
 
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
+    copy_directory("../static", "../public")
 
 
-main()
+if __name__ == "__main__":
+    main()
